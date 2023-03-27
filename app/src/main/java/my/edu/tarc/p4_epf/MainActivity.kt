51d29430ae64322d7da98923f87525580648a263
TARUMT <andrew2645401@gmail.com>
2023-03-27 15:13:51 +0800

    Update profile page.

--- app/src/main/java/my/edu/tarc/p4_epf/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/p4_epf/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +14,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import my.edu.tarc.p4_epf.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -39,27 +42,60 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener{
+                _,destination,_->
+            if(destination.id==R.id.nav_about)
+            {
+                binding.appBarMain.toolbar.menu.findItem(R.id.action_settings).isVisible=false
+                binding.appBarMain.toolbar.menu.findItem(R.id.action_about).isVisible=false
+
+            }
+        }
+
+        //Navigate to Profile
+        //Index 0 = Profile Picture
+        val view = navView.getHeaderView(0);
+        view.setOnClickListener {
+            findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.nav_profile)
+            binding.drawerLayout.closeDrawers()
+        }
+
+        //back press
+        val backPressedCallback = object: OnBackPressedCallback(true)
+        {
+            override fun handleOnBackPressed()
+            {
+                val builder = AlertDialog.Builder(this@MainActivity)
+                builder.setMessage(getString(R.string.exit_message))
+                    .setPositiveButton(getString(R.string.exit),{_,_ -> finish() })
+                    .setNegativeButton(getString(R.string.cancel),{_,_ ->  })
+
+                builder.create().show()
+            }
+        }
+        onBackPressedDispatcher.addCallback(backPressedCallback)
     }
 
-    //Load the Option Menu
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.action_settings) {
-            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
-        }else if (item.itemId == R.id.action_about) {
-            //Implement find navigation controller here
-            findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.nav_about) //Define the about graph
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_settings){
+            Snackbar.make(findViewById(R.id.nav_host_fragment_content_main), R.string.action_settings, Snackbar.LENGTH_SHORT).show()
+        }
+        else if(item.itemId==R.id.action_about)
+        {
+            findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.nav_about)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/my/edu/tarc/p4_epf/ui/profile/ProfileFragment.kt
@@ -0,0 +1,66 @@
+package my.edu.tarc.p4_epf.ui.profile
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import my.edu.tarc.p4_epf.R
+import my.edu.tarc.p4_epf.databinding.FragmentProfileBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment(), MenuProvider {
+
+    var _binding : FragmentProfileBinding? = null;
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        //Add support to Menu
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        menu.findItem(R.id.action_about).isVisible = false
+        menu.findItem(R.id.action_settings).isVisible = false
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        if (menuItem.itemId == R.id.action_save){
+            //TODO: Save profile info
+            Toast.makeText(requireContext(), getString(R.string.profile_save), Toast.LENGTH_SHORT).show()
+
+        }else if (menuItem.itemId == android.R.id.home) {
+            //Handling the Up button
+            findNavController().navigateUp()
+        }
+        return true
+    }
+
+}
